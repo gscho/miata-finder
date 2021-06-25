334590aa3bd394bd49091ea087aea023c89b36ae
gscho <greg.c.schofield@gmail.com>
2021-06-25 13:38:30 -0400

    Filter out image galleries
    Signed-off-by: gscho <greg.c.schofield@gmail.com>

--- find.rb
+++ find.rb
@@ -5,7 +5,9 @@ require 'metainspector'
 KIJIJI_CACHE = "#{ENV['ROOTDIR']}/.cached_miata_links_kijiji".freeze
 
 page = MetaInspector.new('https://www.kijiji.ca/b-cars-trucks/ontario/convertible-mazda-new__used/c174l9004a138a54a49?price=5000__16000&transmission=1')
-current_miata_links = page.links.http.select { |a| a.start_with?('https://www.kijiji.ca/v-cars-trucks/') && a.include?('mazda') }
+current_miata_links = page.links.http.select do |a|
+  a.start_with?('https://www.kijiji.ca/v-cars-trucks/') && a.include?('mazda') && !a.match?(/\?imageNumber=*/)
+end
 previous_miata_links = []
 previous_miata_links = Marshal.load(File.binread(KIJIJI_CACHE)) if File.exist?(KIJIJI_CACHE)
 new_miata_links = current_miata_links - previous_miata_links
